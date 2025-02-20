6d6b5f0bf7b6eb62c86a80c98d27aa80f78ef71e
heethasha <heethasha@gmail.com>
2025-02-19 18:00:36 -0800

    Initial Commit

---
+++ Magic8/Magic8App.swift
@@ -0,0 +1,17 @@
+//
+//  Magic8App.swift
+//  Magic8
+//
+//  Created by Heethasha on 19/2/2025.
+//
+
+import SwiftUI
+
+@main
+struct Magic8App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
